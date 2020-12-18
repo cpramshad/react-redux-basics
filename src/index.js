86d55190834feb4ca24b9a335c3e56a2b50133f6
cpramshad <cpramshad@gmail.com>
2020-12-18 21:57:31 +0400

    [feat] Adding reducer and getting state from store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/reducer.js
@@ -0,0 +1,31 @@
+
+const initialState = { 
+    counter: 0
+}
+
+// Reducer
+const reducer = (state = initialState, action) => {
+    if(action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        };
+    }
+    if(action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        };
+    }
+    if(action.type === 'ADD') {
+        return {
+            counter: state.counter + action.value
+        };
+    }
+    if(action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - action.value
+        };
+    }
+    return state;
+};
+
+export default reducer;
