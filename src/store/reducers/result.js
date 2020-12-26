64130152862128be15017c66e24aa86f2d698005
cpramshad <cpramshad@gmail.com>
2020-12-26 11:49:32 +0400

    [feature] - Added helper function

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,6 +5,11 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, { results: updatedArray});
+}
+
 // Reducer
 const reducer = (state = initialState, action) => {
     switch ( action.type ) {
@@ -15,12 +20,7 @@ const reducer = (state = initialState, action) => {
             // };
             return updateObject(state, { results: state.results.concat({id: new Date(), value: action.result}) });
         case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            // return {
-            //     ...state,
-            //     results: updatedArray
-            // };
-            return updateObject(state, { results: updatedArray});
+            return deleteResult(state, action);
         default:
             return state;            
     }
